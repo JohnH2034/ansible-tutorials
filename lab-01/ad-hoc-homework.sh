0a3cb6dd2e591385d98e8e09fc94450f841beb76
JohnH2034 <jrhoffm@yahoo.com>
2019-04-25 12:13:47 -0600

    Added file backup feature

--- lab-01/ad-hoc-homework.sh
+++ lab-01/ad-hoc-homework.sh
@@ -5,7 +5,7 @@
 echo  -n "Input file name :"
 read filename
 if [ -f $filename ] ; then
-   ansible node01 -i ../inventory/ansible-nodes -m copy -a "src=${filename} dest=/tmp/${filename}"
+   ansible node01 -i ../inventory/ansible-nodes -m copy -a "src=${filename} dest=/tmp/${filename} backup=yes"
 else
    "echo $filename doesn't exist cannot copy"
 fi
